6469b5e13ba6f405131b2724e8944971d5b7a488
Mikael Kalms <mikael@kalms.org>
2017-04-30 00:17:34 +0200

    Spaces to tabs

--- SceneAutoLoader/Assets/Editor/SceneAutoLoader.cs
+++ SceneAutoLoader/Assets/Editor/SceneAutoLoader.cs
@@ -23,188 +23,188 @@ using UnityEditor.SceneManagement;
 [InitializeOnLoad]
 public static class SceneAutoLoader
 {
-    // Static constructor binds a playmode-changed callback.
-    // [InitializeOnLoad] above makes sure this gets execusted.
-    static SceneAutoLoader()
-    {
-        EditorApplication.playmodeStateChanged += OnPlayModeChanged;
-    }
+	// Static constructor binds a playmode-changed callback.
+	// [InitializeOnLoad] above makes sure this gets execusted.
+	static SceneAutoLoader()
+	{
+		EditorApplication.playmodeStateChanged += OnPlayModeChanged;
+	}
 
-    // Menu items to select the "master" scene and control whether or not to load it.
-    [MenuItem("Window/Scene Auto Loader/Select Master Scene...")]
-    private static void SelectMasterScene()
-    {
-        string masterScene = EditorUtility.OpenFilePanel("Select Master Scene", Application.dataPath, "unity");
-        if (!string.IsNullOrEmpty(masterScene))
-        {
-            MasterScene = masterScene;
-            LoadMasterOnPlay = true;
-        }
-    }
+	// Menu items to select the "master" scene and control whether or not to load it.
+	[MenuItem("Window/Scene Auto Loader/Select Master Scene...")]
+	private static void SelectMasterScene()
+	{
+		string masterScene = EditorUtility.OpenFilePanel("Select Master Scene", Application.dataPath, "unity");
+		if (!string.IsNullOrEmpty(masterScene))
+		{
+			MasterScene = masterScene;
+			LoadMasterOnPlay = true;
+		}
+	}
 
-    [MenuItem("Window/Scene Auto Loader/Load Master On Play", true)]
-    private static bool ShowLoadMasterOnPlay()
-    {
-        return !LoadMasterOnPlay;
-    }
-    [MenuItem("Window/Scene Auto Loader/Load Master On Play")]
-    private static void EnableLoadMasterOnPlay()
-    {
-        LoadMasterOnPlay = true;
-    }
+	[MenuItem("Window/Scene Auto Loader/Load Master On Play", true)]
+	private static bool ShowLoadMasterOnPlay()
+	{
+		return !LoadMasterOnPlay;
+	}
+	[MenuItem("Window/Scene Auto Loader/Load Master On Play")]
+	private static void EnableLoadMasterOnPlay()
+	{
+		LoadMasterOnPlay = true;
+	}
 
-    [MenuItem("Window/Scene Auto Loader/Don't Load Master On Play", true)]
-    private static bool ShowDontLoadMasterOnPlay()
-    {
-        return LoadMasterOnPlay;
-    }
-    [MenuItem("Window/Scene Auto Loader/Don't Load Master On Play")]
-    private static void DisableLoadMasterOnPlay()
-    {
-        LoadMasterOnPlay = false;
-    }
+	[MenuItem("Window/Scene Auto Loader/Don't Load Master On Play", true)]
+	private static bool ShowDontLoadMasterOnPlay()
+	{
+		return LoadMasterOnPlay;
+	}
+	[MenuItem("Window/Scene Auto Loader/Don't Load Master On Play")]
+	private static void DisableLoadMasterOnPlay()
+	{
+		LoadMasterOnPlay = false;
+	}
 
-    // Play mode change callback handles the scene load/reload.
-    private static void OnPlayModeChanged()
-    {
-        if (!EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode)
-        {
-            // User pressed play
-
-#if SCENE_AUTO_LOADER_DEBUGGING
-            Debug.Log("Capturing old scene setup...");
-#endif
-            // Check if master scene is part of the current project
-            // If the user switches between projects, the persistet "master scene" setting could potentially
-            //   point to a scene that exists within another project but not this one
-            bool masterSceneExistsInProject = (AssetDatabase.AssetPathToGUID(MasterScene) != "");
-
-            // Store an empty list of scenes by default
-            ScenesInHierarchyView = new SceneSetup[] { };
-
-            // Only proceed if scene loading is active and the master scene exists in the current project
-            if (LoadMasterOnPlay && masterSceneExistsInProject)
-            {
-                // Save current unsaved scenes if necessary
-                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
-                {
-                    // Persist the current list of scenes in the hierarchy view including their status (not loaded, loaded, active).
-                    ScenesInHierarchyView = EditorSceneManager.GetSceneManagerSetup();
-
-                    // User has no unsaved changes at this point; switch to master scene
-                    Scene scene = EditorSceneManager.OpenScene(MasterScene, OpenSceneMode.Single);
-
-                    // If scene switching failed, then cancel play and switch back to the previous set of scenes
-                    if (!scene.IsValid())
-                    {
-                        EditorApplication.isPlaying = false;
-                        EditorApplication.update += ReloadLastScene;
-                    }
-                }
-                else
-                {
-                    // User cancelled the save operation -- cancel play as well.
-                    EditorApplication.isPlaying = false;
-                }
-            }
-        }
-        if (EditorApplication.isPlaying && !EditorApplication.isPlayingOrWillChangePlaymode)
-        {
-            // User pressed stop
-
-            if (ScenesInHierarchyView.Length != 0)
-            {
-                // There is a list of previous scenes available; reload those
-                EditorApplication.update += ReloadLastScene;
-            }
-        }
-    }
-
-    public static void ReloadLastScene()
-    {
-        if (EditorApplication.isPlaying)
-            return;
+	// Play mode change callback handles the scene load/reload.
+	private static void OnPlayModeChanged()
+	{
+		if (!EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode)
+		{
+			// User pressed play
 
 #if SCENE_AUTO_LOADER_DEBUGGING
-        Debug.Log("Reloading editor scene setup...");
+			Debug.Log("Capturing old scene setup...");
 #endif
-        SceneSetup[] scenes = ScenesInHierarchyView;
+			// Check if master scene is part of the current project
+			// If the user switches between projects, the persistet "master scene" setting could potentially
+			//	 point to a scene that exists within another project but not this one
+			bool masterSceneExistsInProject = (AssetDatabase.AssetPathToGUID(MasterScene) != "");
 
-        // Workaround for Unity 5.5.0f3 bug (and possibly in newer versions as well):
-        //
-        // If the first loaded scene in scenes is not active, then RestoreSceneManagerSetup() will not load that scene.
-        // We work around this by ensuring that RestoreSceneManagerSetup() is given a list of scenes where the first loaded scene
-        //  always is active, and if necessary switch the active scene to the appropriate one after the call.
+			// Store an empty list of scenes by default
+			ScenesInHierarchyView = new SceneSetup[] { };
 
-        int activeSceneIndex = Array.FindIndex(scenes, scene => scene.isActive);
-        int firstLoadedSceneIndex = Array.FindIndex(scenes, scene => scene.isLoaded);
+			// Only proceed if scene loading is active and the master scene exists in the current project
+			if (LoadMasterOnPlay && masterSceneExistsInProject)
+			{
+				// Save current unsaved scenes if necessary
+				if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+				{
+					// Persist the current list of scenes in the hierarchy view including their status (not loaded, loaded, active).
+					ScenesInHierarchyView = EditorSceneManager.GetSceneManagerSetup();
 
-        if (activeSceneIndex != firstLoadedSceneIndex)
-        {
-            scenes[firstLoadedSceneIndex].isActive = true;
-            scenes[activeSceneIndex].isActive = false;
-        }
+					// User has no unsaved changes at this point; switch to master scene
+					Scene scene = EditorSceneManager.OpenScene(MasterScene, OpenSceneMode.Single);
 
-        // Restore old scene list in Hierarchy view - not-loaded, loaded, and active scenes
+					// If scene switching failed, then cancel play and switch back to the previous set of scenes
+					if (!scene.IsValid())
+					{
+						EditorApplication.isPlaying = false;
+						EditorApplication.update += ReloadLastScene;
+					}
+				}
+				else
+				{
+					// User cancelled the save operation -- cancel play as well.
+					EditorApplication.isPlaying = false;
+				}
+			}
+		}
+		if (EditorApplication.isPlaying && !EditorApplication.isPlayingOrWillChangePlaymode)
+		{
+			// User pressed stop
 
-        EditorSceneManager.RestoreSceneManagerSetup(scenes);
+			if (ScenesInHierarchyView.Length != 0)
+			{
+				// There is a list of previous scenes available; reload those
+				EditorApplication.update += ReloadLastScene;
+			}
+		}
+	}
 
-        // Workaround part two: switch to the appropriate active scene
+	public static void ReloadLastScene()
+	{
+		if (EditorApplication.isPlaying)
+			return;
 
-        if (activeSceneIndex != firstLoadedSceneIndex)
-            EditorSceneManager.SetActiveScene(EditorSceneManager.GetSceneAt(activeSceneIndex));
+#if SCENE_AUTO_LOADER_DEBUGGING
+		Debug.Log("Reloading editor scene setup...");
+#endif
+		SceneSetup[] scenes = ScenesInHierarchyView;
+
+		// Workaround for Unity 5.5.0f3 bug (and possibly in newer versions as well):
+		//
+		// If the first loaded scene in scenes is not active, then RestoreSceneManagerSetup() will not load that scene.
+		// We work around this by ensuring that RestoreSceneManagerSetup() is given a list of scenes where the first loaded scene
+		//	always is active, and if necessary switch the active scene to the appropriate one after the call.
+
+		int activeSceneIndex = Array.FindIndex(scenes, scene => scene.isActive);
+		int firstLoadedSceneIndex = Array.FindIndex(scenes, scene => scene.isLoaded);
+
+		if (activeSceneIndex != firstLoadedSceneIndex)
+		{
+			scenes[firstLoadedSceneIndex].isActive = true;
+			scenes[activeSceneIndex].isActive = false;
+		}
+
+		// Restore old scene list in Hierarchy view - not-loaded, loaded, and active scenes
+
+		EditorSceneManager.RestoreSceneManagerSetup(scenes);
+
+		// Workaround part two: switch to the appropriate active scene
+
+		if (activeSceneIndex != firstLoadedSceneIndex)
+			EditorSceneManager.SetActiveScene(EditorSceneManager.GetSceneAt(activeSceneIndex));
 
 
-        ScenesInHierarchyView = new SceneSetup[] { };
+		ScenesInHierarchyView = new SceneSetup[] { };
 
-        EditorApplication.update -= ReloadLastScene; ;
-    }
+		EditorApplication.update -= ReloadLastScene; ;
+	}
 
-    // Properties are remembered as editor preferences.
-    private static string cEditorPrefLoadMasterOnPlay { get { return "SceneAutoLoader." + PlayerSettings.productName + ".LoadMasterOnPlay"; } }
-    private static string cEditorPrefMasterScene { get { return "SceneAutoLoader." + PlayerSettings.productName + ".MasterScene"; } }
-    private static string cEditorPrefLoadedScenes { get { return "SceneAutoLoader." + PlayerSettings.productName + ".LoadedScenes"; } }
+	// Properties are remembered as editor preferences.
+	private static string cEditorPrefLoadMasterOnPlay { get { return "SceneAutoLoader." + PlayerSettings.productName + ".LoadMasterOnPlay"; } }
+	private static string cEditorPrefMasterScene { get { return "SceneAutoLoader." + PlayerSettings.productName + ".MasterScene"; } }
+	private static string cEditorPrefLoadedScenes { get { return "SceneAutoLoader." + PlayerSettings.productName + ".LoadedScenes"; } }
 
-    public static bool LoadMasterOnPlay
-    {
-        get { return EditorPrefs.GetBool(cEditorPrefLoadMasterOnPlay, false); }
-        set { EditorPrefs.SetBool(cEditorPrefLoadMasterOnPlay, value); }
-    }
+	public static bool LoadMasterOnPlay
+	{
+		get { return EditorPrefs.GetBool(cEditorPrefLoadMasterOnPlay, false); }
+		set { EditorPrefs.SetBool(cEditorPrefLoadMasterOnPlay, value); }
+	}
 
-    public static string MasterScene
-    {
-        get { return EditorPrefs.GetString(cEditorPrefMasterScene, "Master.unity"); }
-        set { EditorPrefs.SetString(cEditorPrefMasterScene, value); }
-    }
+	public static string MasterScene
+	{
+		get { return EditorPrefs.GetString(cEditorPrefMasterScene, "Master.unity"); }
+		set { EditorPrefs.SetString(cEditorPrefMasterScene, value); }
+	}
 
-    public static SceneSetup[] ScenesInHierarchyView
-    {
-        get
-        {
-            string prefValue = EditorPrefs.GetString(cEditorPrefLoadedScenes, "");
+	public static SceneSetup[] ScenesInHierarchyView
+	{
+		get
+		{
+			string prefValue = EditorPrefs.GetString(cEditorPrefLoadedScenes, "");
 
-            string[] tokens = prefValue.Split('|');
+			string[] tokens = prefValue.Split('|');
 
-            int numScenes = tokens.Length / 3;
+			int numScenes = tokens.Length / 3;
 
-            SceneSetup[] scenes = new SceneSetup[numScenes];
+			SceneSetup[] scenes = new SceneSetup[numScenes];
 
-            for (int i = 0; i < tokens.Length / 3; i++)
-            {
-                scenes[i] = new SceneSetup();
-                scenes[i].isActive = (tokens[i * 3 + 0] != "false");
-                scenes[i].isLoaded = (tokens[i * 3 + 1] != "false");
-                scenes[i].path = tokens[i * 3 + 2];
-            }
-          
-            return scenes;
-        }
+			for (int i = 0; i < tokens.Length / 3; i++)
+			{
+				scenes[i] = new SceneSetup();
+				scenes[i].isActive = (tokens[i * 3 + 0] != "false");
+				scenes[i].isLoaded = (tokens[i * 3 + 1] != "false");
+				scenes[i].path = tokens[i * 3 + 2];
+			}
+		  
+			return scenes;
+		}
 
-        set
-        {
-            string prefValue = string.Join("|", value.Select(scene => (scene.isActive ? "true" : "false") + "|" + (scene.isLoaded ? "true" : "false") + "|" + scene.path).ToArray());
+		set
+		{
+			string prefValue = string.Join("|", value.Select(scene => (scene.isActive ? "true" : "false") + "|" + (scene.isLoaded ? "true" : "false") + "|" + scene.path).ToArray());
 
-            EditorPrefs.SetString(cEditorPrefLoadedScenes, prefValue);
-        }
-    }
+			EditorPrefs.SetString(cEditorPrefLoadedScenes, prefValue);
+		}
+	}
 }
--- SceneAutoLoader/Assets/Editor/SceneAutoLoaderEditorWindow.cs
+++ SceneAutoLoader/Assets/Editor/SceneAutoLoaderEditorWindow.cs
@@ -5,85 +5,85 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 
 /// <summary>
-///  This class adds an Inspector window wrapper for the Scene Auto Loader.
+///	 This class adds an Inspector window wrapper for the Scene Auto Loader.
 /// </summary>
 
 public class SceneAutoLoaderEditorWindow : EditorWindow
 {
-    [MenuItem("Window/Scene Auto Loader/Open Inspector Window")]
-    static void Init()
-    {
-        // Get existing open window or if none, make a new one:
-        SceneAutoLoaderEditorWindow window = (SceneAutoLoaderEditorWindow)EditorWindow.GetWindow(typeof(SceneAutoLoaderEditorWindow));
-        window.Show();
-    }
+	[MenuItem("Window/Scene Auto Loader/Open Inspector Window")]
+	static void Init()
+	{
+		// Get existing open window or if none, make a new one:
+		SceneAutoLoaderEditorWindow window = (SceneAutoLoaderEditorWindow)EditorWindow.GetWindow(typeof(SceneAutoLoaderEditorWindow));
+		window.Show();
+	}
 
-    /// <summary>
-    /// Return a list of all scene files that exist in project
-    /// </summary>
-    private string[] GetScenes()
-    {
-        string[] sceneGuids = AssetDatabase.FindAssets("t:SceneAsset");
-        string[] sceneNames = sceneGuids.Select(sceneGuid => AssetDatabase.GUIDToAssetPath(sceneGuid)).ToArray();
-        return sceneNames;
-    }
+	/// <summary>
+	/// Return a list of all scene files that exist in project
+	/// </summary>
+	private string[] GetScenes()
+	{
+		string[] sceneGuids = AssetDatabase.FindAssets("t:SceneAsset");
+		string[] sceneNames = sceneGuids.Select(sceneGuid => AssetDatabase.GUIDToAssetPath(sceneGuid)).ToArray();
+		return sceneNames;
+	}
 
-    void OnApplicationQuit()
-    {
-        Debug.Log("SceneAutoLoaderEditorWindow: Application ending");
-    }
-    /// <summary>
-    /// Show "Active" checkbox in editor window
-    /// </summary>
-    private bool OnGUI_Active()
-    {
-        bool previousLoadMasterOnPlay = SceneAutoLoader.LoadMasterOnPlay;
+	void OnApplicationQuit()
+	{
+		Debug.Log("SceneAutoLoaderEditorWindow: Application ending");
+	}
+	/// <summary>
+	/// Show "Active" checkbox in editor window
+	/// </summary>
+	private bool OnGUI_Active()
+	{
+		bool previousLoadMasterOnPlay = SceneAutoLoader.LoadMasterOnPlay;
 
-        bool newLoadMasterOnPlay = EditorGUILayout.Toggle("Active", previousLoadMasterOnPlay);
+		bool newLoadMasterOnPlay = EditorGUILayout.Toggle("Active", previousLoadMasterOnPlay);
 
-        if (newLoadMasterOnPlay != previousLoadMasterOnPlay)
-            SceneAutoLoader.LoadMasterOnPlay = newLoadMasterOnPlay;
+		if (newLoadMasterOnPlay != previousLoadMasterOnPlay)
+			SceneAutoLoader.LoadMasterOnPlay = newLoadMasterOnPlay;
 
-        return newLoadMasterOnPlay;
-    }
+		return newLoadMasterOnPlay;
+	}
 
-    /// <summary>
-    /// Show scene dropdown in editor window
-    /// </summary>
-    private void OnGUI_SelectMasterScene()
-    {
-        string previousMasterScene = SceneAutoLoader.MasterScene;
+	/// <summary>
+	/// Show scene dropdown in editor window
+	/// </summary>
+	private void OnGUI_SelectMasterScene()
+	{
+		string previousMasterScene = SceneAutoLoader.MasterScene;
 
-        string[] scenes = new string[] { "<No scene chosen>" }.Concat(GetScenes()).ToArray();
+		string[] scenes = new string[] { "<No scene chosen>" }.Concat(GetScenes()).ToArray();
 
-        // Map from scene name to index in list
-        // If previousMasterScene cannot be found in the list of scenes (or it is set to ""), default to the <No scene chosen> option
-        int previousSelectedIndex = Math.Max(Array.IndexOf(scenes, previousMasterScene), 0);
+		// Map from scene name to index in list
+		// If previousMasterScene cannot be found in the list of scenes (or it is set to ""), default to the <No scene chosen> option
+		int previousSelectedIndex = Math.Max(Array.IndexOf(scenes, previousMasterScene), 0);
 
-        // Hack: Forward slashes in paths like "Assets/Scenes/MyScene.unity" are converted to "division slash" characters + spacing for display
-        // This is to prevent Unity from splitting the dropdown into sub-dropdowns at each forward slash
-        //
-        // Other people facing the same problem:
-        //   http://answers.unity3d.com/questions/46676/how-can-i-put-a-list-of-filenames-into-an-editor-m.html
-        //   http://answers.unity3d.com/questions/398495/can-genericmenu-item-content-display-.html
-        // Suggestion to have the separator character user-configurable:
-        //   https://feedback.unity3d.com/suggestions/genericmenu-submenu-delimiter-character-override
-        string[] scenesDisplayNames = scenes.Select(scene => scene.Replace("/", " \u2215 ").Replace(".unity", "")).ToArray();
-        int newSelectedIndex = EditorGUILayout.Popup("Scene to auto load", previousSelectedIndex, scenesDisplayNames);
+		// Hack: Forward slashes in paths like "Assets/Scenes/MyScene.unity" are converted to "division slash" characters + spacing for display
+		// This is to prevent Unity from splitting the dropdown into sub-dropdowns at each forward slash
+		//
+		// Other people facing the same problem:
+		//	 http://answers.unity3d.com/questions/46676/how-can-i-put-a-list-of-filenames-into-an-editor-m.html
+		//	 http://answers.unity3d.com/questions/398495/can-genericmenu-item-content-display-.html
+		// Suggestion to have the separator character user-configurable:
+		//	 https://feedback.unity3d.com/suggestions/genericmenu-submenu-delimiter-character-override
+		string[] scenesDisplayNames = scenes.Select(scene => scene.Replace("/", " \u2215 ").Replace(".unity", "")).ToArray();
+		int newSelectedIndex = EditorGUILayout.Popup("Scene to auto load", previousSelectedIndex, scenesDisplayNames);
 
-        // Map from index in list to full scene path
-        // If <No scene chosen> is chosen, translate to empty path, otherwise keep as-is
-        string newMasterScene = (newSelectedIndex > 0) ? scenes[newSelectedIndex] : "";
+		// Map from index in list to full scene path
+		// If <No scene chosen> is chosen, translate to empty path, otherwise keep as-is
+		string newMasterScene = (newSelectedIndex > 0) ? scenes[newSelectedIndex] : "";
 
-        if (newMasterScene != previousMasterScene)
-            SceneAutoLoader.MasterScene = newMasterScene;
-    }
+		if (newMasterScene != previousMasterScene)
+			SceneAutoLoader.MasterScene = newMasterScene;
+	}
 
-    void OnGUI()
-    {
-        bool isActive = OnGUI_Active();
+	void OnGUI()
+	{
+		bool isActive = OnGUI_Active();
 
-        if (isActive)
-            OnGUI_SelectMasterScene();
-    }
+		if (isActive)
+			OnGUI_SelectMasterScene();
+	}
 }
