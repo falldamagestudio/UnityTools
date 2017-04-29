4f761c15cfef516a37e4830b9d897e5928a9a7d1
Mikael Kalms <mikael@kalms.org>
2017-04-30 00:01:06 +0200

    Spaces to tabs in source code

--- DisableAssemblyReloadInPlayMode/Assets/Editor/DisableAssemblyReloadInPlayMode.cs
+++ DisableAssemblyReloadInPlayMode/Assets/Editor/DisableAssemblyReloadInPlayMode.cs
@@ -3,24 +3,24 @@ using UnityEditor;
 [InitializeOnLoad]
 public class DisableAssemblyReloadInPlayMode
 {
-    private static bool Locked;
+	private static bool Locked;
 
 	static DisableAssemblyReloadInPlayMode()
 	{
-        EditorApplication.playmodeStateChanged += OnPlaymodeStateChanged;
+		EditorApplication.playmodeStateChanged += OnPlaymodeStateChanged;
 	}
 
 	private static void OnPlaymodeStateChanged()
 	{
-        if (EditorApplication.isPlaying && !Locked)
-        {
-            EditorApplication.LockReloadAssemblies();
-            Locked = true;
-        }
-        else if (!EditorApplication.isPlaying && Locked)
-        {
-            EditorApplication.UnlockReloadAssemblies();
-            Locked = false;
-        }
- 	}
+		if (EditorApplication.isPlaying && !Locked)
+		{
+			EditorApplication.LockReloadAssemblies();
+			Locked = true;
+		}
+		else if (!EditorApplication.isPlaying && Locked)
+		{
+			EditorApplication.UnlockReloadAssemblies();
+			Locked = false;
+		}
+	}
 }
